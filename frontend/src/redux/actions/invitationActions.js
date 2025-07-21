45d4ee93224055f4a4893034889a9aff588ca5fc
berlindinesh <iamdineshsundar@gmail.com>
2025-07-21 15:29:16 +0530

    changes

--- frontend/src/redux/actions/invitationActions.js
+++ frontend/src/redux/actions/invitationActions.js
@@ -22,7 +22,7 @@ export const fetchInvitations = () => async (dispatch) => {
   dispatch({ type: FETCH_INVITATIONS_REQUEST });
   
   try {
-    const response = await api.get('/api/invitations');
+    const response = await api.get('/invitations');
     
     dispatch({
       type: FETCH_INVITATIONS_SUCCESS,
@@ -47,7 +47,7 @@ export const inviteUser = (userData) => async (dispatch) => {
       'X-Company-Code': localStorage.getItem('companyCode')
     });
     
-    const response = await api.post('/api/invitations', userData);
+    const response = await api.post('/invitations', userData);
     console.log('Invitation response:', response.data);
     
     dispatch({
@@ -87,7 +87,7 @@ export const resendInvitation = (invitationId) => async (dispatch) => {
   dispatch({ type: RESEND_INVITATION_REQUEST });
   
   try {
-    const response = await api.post(`/api/invitations/${invitationId}/resend`, {});
+    const response = await api.post(`/invitations/${invitationId}/resend`, {});
     
     dispatch({
       type: RESEND_INVITATION_SUCCESS,
@@ -113,7 +113,7 @@ export const cancelInvitation = (invitationId) => async (dispatch) => {
   dispatch({ type: CANCEL_INVITATION_REQUEST });
   
   try {
-    const response = await api.delete(`/api/invitations/${invitationId}`);
+    const response = await api.delete(`/invitations/${invitationId}`);
     
     dispatch({
       type: CANCEL_INVITATION_SUCCESS,
@@ -140,7 +140,7 @@ export const checkInvitationUpdates = () => async (dispatch, getState) => {
   const lastCheck = invitations.lastCheck;
   
   try {
-    const response = await api.get(`/api/invitations?lastCheck=${lastCheck}`);
+    const response = await api.get(`/invitations?lastCheck=${lastCheck}`);
     
     if (response.data.hasUpdates) {
       dispatch({
@@ -155,137 +155,3 @@ export const checkInvitationUpdates = () => async (dispatch, getState) => {
     console.error('Error checking invitation updates:', error);
   }
 };
-
-
-// import api from '../../services/api';
-// import {
-//   FETCH_INVITATIONS_REQUEST,
-//   FETCH_INVITATIONS_SUCCESS,
-//   FETCH_INVITATIONS_FAILURE,
-//   INVITE_USER_REQUEST,
-//   INVITE_USER_SUCCESS,
-//   INVITE_USER_FAILURE,
-//   RESEND_INVITATION_REQUEST,
-//   RESEND_INVITATION_SUCCESS,
-//   RESEND_INVITATION_FAILURE,
-//   CANCEL_INVITATION_REQUEST,
-//   CANCEL_INVITATION_SUCCESS,
-//   CANCEL_INVITATION_FAILURE
-// } from './types';
-
-// // Fetch all invitations
-// export const fetchInvitations = () => async (dispatch) => {
-//   dispatch({ type: FETCH_INVITATIONS_REQUEST });
-  
-//   try {
-//     const response = await api.get('/api/invitations');
-    
-//     dispatch({
-//       type: FETCH_INVITATIONS_SUCCESS,
-//       payload: response.data
-//     });
-//   } catch (error) {
-//     dispatch({
-//       type: FETCH_INVITATIONS_FAILURE,
-//       payload: error.response?.data?.message || 'Error fetching invitations'
-//     });
-//   }
-// };
-
-// // Invite a new user
-// export const inviteUser = (userData) => async (dispatch) => {
-//   dispatch({ type: INVITE_USER_REQUEST });
-  
-//   try {
-//     console.log('Sending invitation request with data:', userData);
-//     console.log('API headers:', {
-//       Authorization: `Bearer ${localStorage.getItem('token')}`,
-//       'X-Company-Code': localStorage.getItem('companyCode')
-//     });
-    
-//     const response = await api.post('/api/invitations', userData);
-//     console.log('Invitation response:', response.data);
-    
-//     dispatch({
-//       type: INVITE_USER_SUCCESS,
-//       payload: response.data
-//     });
-    
-//     // Refresh invitation list after creating a new invitation
-//     dispatch(fetchInvitations());
-    
-//     return response.data;
-//   } catch (error) {
-//     console.error('Detailed invitation error:', {
-//       message: error.message,
-//       response: error.response?.data,
-//       status: error.response?.status,
-//       statusText: error.response?.statusText,
-//       config: {
-//         url: error.config?.url,
-//         method: error.config?.method,
-//         baseURL: error.config?.baseURL,
-//         headers: error.config?.headers
-//       }
-//     });
-    
-//     dispatch({
-//       type: INVITE_USER_FAILURE,
-//       payload: error.response?.data?.message || 'Error inviting user'
-//     });
-    
-//     throw error;
-//   }
-// };
-
-// // Resend invitation
-// export const resendInvitation = (invitationId) => async (dispatch) => {
-//   dispatch({ type: RESEND_INVITATION_REQUEST });
-  
-//   try {
-//     const response = await api.post(`/invitations/${invitationId}/resend`, {});
-    
-//     dispatch({
-//       type: RESEND_INVITATION_SUCCESS,
-//       payload: response.data
-//     });
-    
-//     // Refresh invitation list after resending
-//     dispatch(fetchInvitations());
-    
-//     return response.data;
-//   } catch (error) {
-//     dispatch({
-//       type: RESEND_INVITATION_FAILURE,
-//       payload: error.response?.data?.message || 'Error resending invitation'
-//     });
-    
-//     throw error;
-//   }
-// };
-
-// // Cancel invitation
-// export const cancelInvitation = (invitationId) => async (dispatch) => {
-//   dispatch({ type: CANCEL_INVITATION_REQUEST });
-  
-//   try {
-//     const response = await api.delete(`/invitations/${invitationId}`);
-    
-//     dispatch({
-//       type: CANCEL_INVITATION_SUCCESS,
-//       payload: { invitationId, ...response.data }
-//     });
-    
-//     // Refresh invitation list after cancelling
-//     dispatch(fetchInvitations());
-    
-//     return response.data;
-//   } catch (error) {
-//     dispatch({
-//       type: CANCEL_INVITATION_FAILURE,
-//       payload: error.response?.data?.message || 'Error cancelling invitation'
-//     });
-    
-//     throw error;
-//   }
-// };
--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -29,7 +29,7 @@ export const fetchUsers = () => async (dispatch) => {
   dispatch({ type: FETCH_USERS_REQUEST });
   
   try {
-    const response = await api.get('/api/users');
+    const response = await api.get('/users');
     
     dispatch({
       type: FETCH_USERS_SUCCESS,
@@ -48,7 +48,7 @@ export const updateUserRole = (userId, roleData) => async (dispatch) => {
   dispatch({ type: UPDATE_USER_ROLE_REQUEST });
   
   try {
-    const response = await api.put(`/api/users/${userId}/role`, roleData);
+    const response = await api.put(`/users/${userId}/role`, roleData);
     
     dispatch({
       type: UPDATE_USER_ROLE_SUCCESS,
@@ -74,7 +74,7 @@ export const updateUserStatus = (userId, statusData) => async (dispatch) => {
   dispatch({ type: UPDATE_USER_STATUS_REQUEST });
   
   try {
-    const response = await api.put(`/api/users/${userId}/status`, statusData);
+    const response = await api.put(`/users/${userId}/status`, statusData);
     
     dispatch({
       type: UPDATE_USER_STATUS_SUCCESS,
@@ -100,7 +100,7 @@ export const updateUserProfile = (userId, profileData) => async (dispatch) => {
   dispatch({ type: UPDATE_USER_PROFILE_REQUEST });
   
   try {
-    const response = await api.put(`/api/users/${userId}/profile`, profileData);
+    const response = await api.put(`/users/${userId}/profile`, profileData);
     
     dispatch({
       type: UPDATE_USER_PROFILE_SUCCESS,
@@ -126,7 +126,7 @@ export const deleteUser = (userId) => async (dispatch) => {
   dispatch({ type: DELETE_USER_REQUEST });
   
   try {
-    const response = await api.delete(`/api/users/${userId}`);
+    const response = await api.delete(`/users/${userId}`);
     
     dispatch({
       type: DELETE_USER_SUCCESS,
@@ -152,7 +152,7 @@ export const resetUserPassword = (userId) => async (dispatch) => {
   dispatch({ type: RESET_USER_PASSWORD_REQUEST });
   
   try {
-    const response = await api.post(`/api/users/${userId}/reset-password`);
+    const response = await api.post(`/users/${userId}/reset-password`);
     
     dispatch({
       type: RESET_USER_PASSWORD_SUCCESS,
@@ -193,114 +193,3 @@ export const changePassword = (passwordData) => async (dispatch) => {
   }
 };
 
-
-// import api from '../../services/api';
-// import userApi from '../../api/userApi';
-// import {
-//   FETCH_USERS_REQUEST,
-//   FETCH_USERS_SUCCESS,
-//   FETCH_USERS_FAILURE,
-//   UPDATE_USER_ROLE_REQUEST,
-//   UPDATE_USER_ROLE_SUCCESS,
-//   UPDATE_USER_ROLE_FAILURE,
-//   UPDATE_USER_STATUS_REQUEST,
-//   UPDATE_USER_STATUS_SUCCESS,
-//   UPDATE_USER_STATUS_FAILURE,
-//   CHANGE_PASSWORD_REQUEST,
-//   CHANGE_PASSWORD_SUCCESS,
-//   CHANGE_PASSWORD_FAILURE
-// } from './types';
-
-// // Fetch all users
-// export const fetchUsers = () => async (dispatch) => {
-//   dispatch({ type: FETCH_USERS_REQUEST });
-  
-//   try {
-//     const response = await api.get('/api/roles/users');
-    
-//     dispatch({
-//       type: FETCH_USERS_SUCCESS,
-//       payload: response.data
-//     });
-//   } catch (error) {
-//     dispatch({
-//       type: FETCH_USERS_FAILURE,
-//       payload: error.response?.data?.message || 'Error fetching users'
-//     });
-//   }
-// };
-
-// // Update user role
-// export const updateUserRole = (userId, roleData) => async (dispatch) => {
-//   dispatch({ type: UPDATE_USER_ROLE_REQUEST });
-  
-//   try {
-//     const response = await api.put(`/api/roles/users/${userId}/role`, roleData);
-    
-//     dispatch({
-//       type: UPDATE_USER_ROLE_SUCCESS,
-//       payload: response.data
-//     });
-    
-//     // Refresh user list after update
-//     dispatch(fetchUsers());
-    
-//     return response.data;
-//   } catch (error) {
-//     dispatch({
-//       type: UPDATE_USER_ROLE_FAILURE,
-//       payload: error.response?.data?.message || 'Error updating user role'
-//     });
-    
-//     throw error;
-//   }
-// };
-
-// // Update user status
-// export const updateUserStatus = (userId, statusData) => async (dispatch) => {
-//   dispatch({ type: UPDATE_USER_STATUS_REQUEST });
-  
-//   try {
-//     const response = await api.put(`/api/roles/users/${userId}/status`, statusData);
-    
-//     dispatch({
-//       type: UPDATE_USER_STATUS_SUCCESS,
-//       payload: response.data
-//     });
-    
-//     // Refresh user list after update
-//     dispatch(fetchUsers());
-    
-//     return response.data;
-//   } catch (error) {
-//     dispatch({
-//       type: UPDATE_USER_STATUS_FAILURE,
-//       payload: error.response?.data?.message || 'Error updating user status'
-//     });
-    
-//     throw error;
-//   }
-// };
-
-// // Change password action
-// export const changePassword = (passwordData) => async (dispatch) => {
-//   dispatch({ type: CHANGE_PASSWORD_REQUEST });
-  
-//   try {
-//     const response = await userApi.changePassword(passwordData);
-    
-//     dispatch({
-//       type: CHANGE_PASSWORD_SUCCESS,
-//       payload: response.message
-//     });
-    
-//     return Promise.resolve(response);
-//   } catch (error) {
-//     dispatch({
-//       type: CHANGE_PASSWORD_FAILURE,
-//       payload: error.message || 'Error changing password'
-//     });
-    
-//     return Promise.reject(error);
-//   }
-// };
